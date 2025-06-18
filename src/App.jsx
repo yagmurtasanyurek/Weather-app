de9a00a58a946e22b056f0d6d5341bb6adf0f0c5
Yağmur Taşanyürek <yagmurtasanyurek@gmail.com>
2025-06-18 22:47:19 +0300

    Add: user location,reverse geocoding

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,34 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function App() {
+  const getData = async () => {
+    try {
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+
+      const { latitude: lat, longitude: long } = position.coords;
+
+      const response = await axios.get(
+        "https://api-bdc.net/data/reverse-geocode",
+        {
+          params: {
+            latitude: lat,
+            longitude: long,
+            key: "bdc_4dc7684a774941f493463004ab369404",
+          },
+        }
+      );
+    } catch (error) {
+      console.error("Error fetching geolocation or reverse geocoding:", error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return <div>App</div>;
 }
 export default App;
