e5df72bc0297f0027754613a16c722417ab74b14
Yağmur Taşanyürek <yagmurtasanyurek@gmail.com>
2025-09-16 21:37:22 +0300

    change in ui

--- src/skeletons/MainWeatherSkeleton.jsx
+++ src/skeletons/MainWeatherSkeleton.jsx
@@ -3,10 +3,13 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 export function MainWeatherSkeleton() {
   return (
-    <div className="flex flex-col sm:flex-row justify-between gap-y-10 items-center mb-20">
+    <div className="flex flex-col sm:flex-row justify-between gap-y-10 items-center mb-20 ">
       {/* Current temperature */}
       <div className=" flex flex-col sm:flex-row items-center shrink-0 mb-2 ">
-        <Skeleton circle className="w-40 h-40 md:w-64 md:h-64 mb-2" />
+        <Skeleton
+          circle
+          className="w-40 h-40 md:w-64 md:h-64 mb-2 sm:mb-0 sm:mr-4"
+        />
 
         <div>
           <Skeleton width={110} height={60} className="mb-4" />
